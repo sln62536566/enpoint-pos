916470fa875160bf18cc17fa82a636943717e943
Enpoint POS Developer <sln62536566@gmail.com>
2026-05-07 01:02:13 +0800

    修正 Firebase databaseURL

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ admin.initializeApp({
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     privateKey: (process.env.FIREBASE_PRIVATE_KEY || "").replace(/\\n/g, '\n')
   }),
-  databaseURL: "https://enpoint-api-default-rtdb.firebaseio.com/"
+  databaseURL: "https://enpoint-pos-default-rtdb.asia-southeast1.firebasedatabase.app/"
 });
 
 const db = admin.database();
