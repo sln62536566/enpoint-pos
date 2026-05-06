f5ad886231a9cbd34d5311b2c5e208f175d9f5f3
Enpoint POS Developer <sln62536566@gmail.com>
2026-05-07 02:46:20 +0800

    fix: firebase add get/set export for order counter

--- public/firebase.js
+++ public/firebase.js
@@ -1,6 +1,14 @@
-// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getDatabase, ref, push, onValue, update, remove } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+import {
+  getDatabase,
+  ref,
+  push,
+  onValue,
+  update,
+  remove,
+  get,
+  set
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBz5ixYBa6q6yB4uObJNdUVqDuL8X4uyw0",
@@ -15,4 +23,13 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
-export { db, ref, push, onValue, update, remove };
+export {
+  db,
+  ref,
+  push,
+  onValue,
+  update,
+  remove,
+  get,
+  set
+};
