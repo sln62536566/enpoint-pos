e73e64d53dfeb0980f2568e5c15df698d50601e6
Enpoint POS Developer <sln62536566@gmail.com>
2026-05-06 23:25:55 +0800

    完成 訂單流程 + 廚房系統 v1

--- public/firebase.js
+++ public/firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebas
 import { getDatabase, ref, push, onValue, update, remove } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "你的KEY",
-  authDomain: "你的DOMAIN",
-  databaseURL: "你的DB",
-  projectId: "你的ID",
-  storageBucket: "你的BUCKET",
-  messagingSenderId: "你的SENDER",
-  appId: "你的APPID"
+  apiKey: "AIzaSyBz5ixYBa6q6yB4uObJNdUVqDuL8X4uyw0",
+  authDomain: "enpoint-pos.firebaseapp.com",
+  databaseURL: "https://enpoint-pos-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "enpoint-pos",
+  storageBucket: "enpoint-pos.firebasestorage.app",
+  messagingSenderId: "1085275616655",
+  appId: "1:1085275616655:web:96a86e2d6bf89d2717c7fa",
 };
 
 const app = initializeApp(firebaseConfig);
